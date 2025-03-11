487824c9d06fe1edc42ec3d8030b0d40d61d49cd
lukaszCode902 <kontakt.kwiecien@gmail.com>
2025-03-11 18:15:17 +0100

    dodanie typów danych string/number/boolean + README.md

--- python/main.py
+++ python/main.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+# Typ danych string - ćwiczenia
+
+# Podstawowy zapis stringa w pojedyńczym " " umożliwia przechowanie tekstu (stringa) w jednnej linii. 
+#  """ Test
+#       Mój tekst - trzykrotny znak """ umożliwia przechowywanie stringa w formacie wielowierszowym.
+# # """
+
+description = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+
+
+sed do eiusmod tempor incididunt
+
+
+ut labore et dolore magna aliqua"""
+
+age = "30"
+
+# Metody jakich możemy uzywać na typie danych string.
+newDescription = description[0:5]
+print(newDescription)
